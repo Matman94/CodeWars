036f3c1791ebf59649dc006fa82f1614ae81197d
Mateusz <mateusz.drygala@gmail.com>
2018-07-09 13:21:26 +0200

    -poprawki

---
+++ OnesAndZeros/OnesAndZeros/Kata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnesAndZeros
+{
+    public class Kata
+    {
+        public static int binaryArrayToNumber(int [] BinaryArray)
+        {
+            int sum = 0;
+            int n = 0; // which power - która potęga
+
+            int countIteration = 0;
+
+            //code here
+            for (int i=BinaryArray.Length-1; i >= 0; i--)
+            {
+                Console.WriteLine("Jest to {0} iteracja pętli for", countIteration);
+
+                if (BinaryArray[i] == 1)
+                {
+                    int multiple = 1;
+                    
+                    if (n > 0)
+                    {
+                        for (int j = 0; j <= n-1; j++)
+                        {
+                           
+                            multiple *= 2;
+                            
+                        }
+                    }
+                    
+                    sum += multiple;
+
+                    Console.WriteLine("----------------------------------------");
+                    Console.WriteLine("Aktualna potęga: {0}", n);
+                    Console.WriteLine("Aktualna wartość mnożenia: {0}", multiple);
+                    Console.WriteLine("Aktualna suma: {0}", sum);
+                    Console.WriteLine("----------------------------------------");
+
+
+                    n++;
+                }
+                else
+                {
+                    n++;
+                }
+
+                countIteration++;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ OnesAndZeros/OnesAndZeros/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnesAndZeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] arrayTests = { 0, 0, 1, 0, 1, 0, 1, 1 };
+            //int result = Kata.binaryArrayToNumber(arrayTests);
+
+            int[] arrayTests = { 1, 1, 1, 0, 1, 1, 1, 1 };
+            int result = Kata.binaryArrayToNumber(arrayTests);
+
+            Console.WriteLine("tablica - liczba dwójkowo: {0}", string.Join(",", arrayTests));
+            Console.WriteLine("================================================================");
+            Console.WriteLine("================================================================");
+            Console.WriteLine("Wynik: {0}", result);
+            Console.WriteLine("================================================================");
+            Console.WriteLine("================================================================");
+            Console.ReadLine();
+        }
+    }
+}
